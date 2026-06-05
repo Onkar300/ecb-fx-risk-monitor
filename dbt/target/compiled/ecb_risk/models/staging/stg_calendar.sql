082591d330b463652db16b083a0cff6ddc8e18bd
Onkar30 <sahaonkar30@gmail.com>
2026-06-05 21:14:21 +0200

    chore: exclude dbt build artifacts from version control

--- dbt/target/compiled/ecb_risk/models/staging/stg_calendar.sql
+++
@@ -1,6 +0,0 @@
--- Business-day calendar from 2015-01-01 to today. Used to forward-fill HICP.
-
-
-select d::date as cal_date
-from generate_series(date '2015-01-01', current_date, interval '1 day') as d
-where extract(isodow from d) < 6
